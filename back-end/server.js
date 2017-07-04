46eaf7016966ad9daa58babd0cf68810d209e46c
heshamelmasry77 <heshamelmasry@outlook.com>
2017-07-04 13:47:07 +0200

    Fixing cores and test the api , adding the connection between the api and the front end app

--- back-end/server.js
+++ back-end/server.js
@@ -4,6 +4,9 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongo = require('mongodb').MongoClient;
+
+var database;
 
 app.use(bodyParser.json());
 
@@ -15,7 +18,16 @@ app.use(function (req, res, next) {
 // creating an end point
 app.post('/api/message', function (req, res) {
     console.log(req.body);
+    database.collection('messages').insertOne(req.body);
+
     res.status(200);
+});
+
+mongo.connect("mongodb://localhost:27017/testOne", function (err, db) {
+    if (!err) {
+        console.log("database connected");
+        database = db;
+    }
 });
 
 var server = app.listen(5000, function () {
