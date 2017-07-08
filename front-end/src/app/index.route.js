56e92006dc2be4adde37b4c7d95080119393bcf6
heshamelmasry77 <heshamelmasry@outlook.com>
2017-07-08 14:19:49 +0200

    Adding register and login form, changing to ui-sref, adding auth controller and registering it

--- front-end/src/app/index.route.js
+++ front-end/src/app/index.route.js
@@ -1,4 +1,4 @@
-export function routerConfig ($stateProvider, $urlRouterProvider) {
+export function routerConfig($stateProvider, $urlRouterProvider) {
   'ngInject';
   $stateProvider
     .state('home', {
@@ -6,6 +6,12 @@ export function routerConfig ($stateProvider, $urlRouterProvider) {
       templateUrl: 'app/main/main.html',
       controller: 'MainController',
       controllerAs: 'main'
+    })
+    .state('auth', {
+      url: '/auth',
+      templateUrl: 'app/auth/auth.html',
+      controller: 'AuthController',
+      controllerAs: 'auth'
     });
 
   $urlRouterProvider.otherwise('/');
--- front-end/src/app/main/main.controller.js
+++ front-end/src/app/main/main.controller.js
@@ -12,12 +12,12 @@ export class MainController {
     this.$http.get('http://localhost:5000/api/message').then(function (result) {
       // console.log(result);
       vm.messages = result.data;
-    });
+    })
 
-  };
+  }
 
   postMessage() {
     this.$http.post('http://localhost:5000/api/message', {msg: this.message});
-  };
+  }
 }
 
