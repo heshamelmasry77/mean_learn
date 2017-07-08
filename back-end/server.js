35a80ed41870f6c8395607e0fc130ddb79e8d9b1
heshamelmasry77 <heshamelmasry@outlook.com>
2017-07-08 13:02:29 +0200

    Adding mongoose, adding a get request, adding get messages function, adding a schema for messages collection

--- back-end/server.js
+++ back-end/server.js
@@ -4,9 +4,13 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mongo = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
 
 var database;
+// the name of it
+var Message = mongoose.model('Message', {
+    msg: String
+});
 
 app.use(bodyParser.json());
 
@@ -15,18 +19,29 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
 });
+
+
+// end point to get messages
+app.get('/api/message', GetMessages);
+
 // creating an end point
 app.post('/api/message', function (req, res) {
     console.log(req.body);
-    database.collection('messages').insertOne(req.body);
-
+    var message = new Message(req.body);
+    message.save();
     res.status(200);
 });
 
-mongo.connect("mongodb://localhost:27017/testOne", function (err, db) {
+
+function GetMessages(req, res) {
+    Message.find({}).exec(function (err, result) {
+        res.send(result);
+    });
+}
+
+mongoose.connect("mongodb://localhost:27017/testOne", function (err, db) {
     if (!err) {
         console.log("database connected");
-        database = db;
     }
 });
 
