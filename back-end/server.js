8e61707d54e78a91747322c17769e4c7ca6be05c
heshamelmasry77 <heshamelmasry@outlook.com>
2017-07-09 15:45:31 +0200

    Creating controller folder for my routes, creating auth and message
    Requiring my controllers

--- back-end/server.js
+++ back-end/server.js
@@ -7,10 +7,9 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 var database;
-// the name of it
-var Message = mongoose.model('Message', {
-    msg: String
-});
+
+var auth = require('./controllers/auth');
+var message = require('./controllers/message');
 
 app.use(bodyParser.json());
 
@@ -22,25 +21,13 @@ app.use(function (req, res, next) {
 
 
 // end point to get messages
-app.get('/api/message', GetMessages);
+app.get('/api/message', message.get);
 
 // creating an end point
-app.post('/api/message', function (req, res) {
-    console.log(req.body);
-    var message = new Message(req.body);
-    message.save();
-    res.status(200);
-});
+app.post('/api/message', message.post);
 
-app.post('/auth/register', function (req, res) {
-    console.log(req.body);
-});
+app.post('/auth/register', auth.register);
 
-function GetMessages(req, res) {
-    Message.find({}).exec(function (err, result) {
-        res.send(result);
-    });
-}
 
 mongoose.connect("mongodb://localhost:27017/testOne", function (err, db) {
     if (!err) {
