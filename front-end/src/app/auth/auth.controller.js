075ac755ba87e319c4f9863ff97f4d1ecba622b9
heshamelmasry77 <heshamelmasry@outlook.com>
2017-07-17 14:40:29 +0200

    Adding login function and ng submit for the login function

--- front-end/src/app/auth/auth.controller.js
+++ front-end/src/app/auth/auth.controller.js
@@ -12,6 +12,12 @@ export class AuthController {
       vm.$auth.setToken(token);
     });
   }
+  login() {
+    var vm = this;
+    this.$auth.login(this.login.user).then(function (token) {
+      vm.$auth.setToken(token);
+    });
+  }
 }
 
 
